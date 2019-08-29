ac9ac7c34ccc8d65cc37af1b56aa8cdda85ad102
Geumgyu <qkrrmarb@naver.com>
2019-08-30 01:12:22 +0900

    Updates

---
+++ precache-manifest.fe2263bc68cb6c2fea669761f6b96a10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cb7b620961f12e80eabb9cff0d3c4b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "12962aebd94283aaa43d",
+    "url": "/movie_app/static/css/main.10a8dfd0.chunk.css"
+  },
+  {
+    "revision": "e183bcee449302b2a05c",
+    "url": "/movie_app/static/js/2.6196da2f.chunk.js"
+  },
+  {
+    "revision": "12962aebd94283aaa43d",
+    "url": "/movie_app/static/js/main.f24e688f.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  }
+]);
